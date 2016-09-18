9857aea09fe281a71aad08038209db31db391670
kyle uyehara <kuyehara1@gmail.com>
2016-09-18 12:35:28 -1000

    scrap pure functions and implement an impure solution

--- inventoryUpdate.js
+++ inventoryUpdate.js
@@ -1,48 +1,65 @@
 function updateInventory(arr1, arr2) {
-  // All inventory must be accounted for or you're fired!
-
-  //INPUT VALIDATION
-  //CHECK IF THERE ARE TWO ARGUMENTS
+  //**Input Validation**
+  //Check for two arguments
   if(arguments.length !== 2) {
-    var err = new Error('Oops! We were expecting two arguments and instead see ' + arguments.length);
-    throw err;
+    var err1 = new Error('Oops! We were expecting two arguments and instead see ' + arguments.length);
+    throw err1;
   }
-  //CHECK IF THE ARGUMENTS ARE ARRAYS
+
+  //Check argument types are arrays
   for(var key in arguments) {
     var currArr = arguments[key];
 
     if(!Array.isArray(currArr)) {
-      var err = new Error(currArr + ' is not an array');
-      throw err;
+      var err2 = new Error(currArr + ' is not an array');
+      throw err2;
     }
 
+    //Check arrays are two-dimensional with a number and string in that order
     currArr.forEach(function(element) {
-      if(typeof element[0] !== 'number' && typeof element[1] !== 'string') {
-        var err = new Error(element + ' is not properly')
+      if(typeof element[0] !== 'number' || typeof element[1] !== 'string') {
+        var err3 = new Error(element + ' is improperly composed');
+        throw err3;
       }
     });
+  }
 
-    if(flag) {
-      var err = one
+  function invUpdater(currInv, addInv) {
+    var unMatched;
+    for(var i = 0; i < currInv.length; i++) {
+      var match = addInv.find(itemMatcher);
+      console.log(match, 'MATCH');
+      if(match === undefined) {
+        currInv.push(unMatched);
+      } else {
+        currInv[i] = [currInv[i][0] + match[0], match[1]];
+      }
+
+    }
+
+    function itemMatcher(addItem) {
+      if(addItem[1] === currInv[i][1])
+        return addItem[1] === currInv[i][1];
+      }
+      unMatched = addItem;
     }
   }
-  //CHECK IF INNER ARRAYS ARE COMPOSED OF TWO ARGUMENTS, FIRST A NUMBER, SECOND A STRING
-
+  invUpdater(arr1, arr2);
 }
 
 // Example inventory lists
-var curInv = [
+var currInv = [
     [21, "Bowling Ball"],
     [2, "Dirty Sock"],
     [1, "Hair Pin"],
     [5, "Microphone"]
 ];
 
-var newInv = [
+var addInv = [
     [2, "Hair Pin"],
     [3, "Half-Eaten Apple"],
     [67, "Bowling Ball"],
     [7, "Toothpaste"]
 ];
 
-updateInventory(curInv, newInv);
+updateInventory(currInv, addInv);
