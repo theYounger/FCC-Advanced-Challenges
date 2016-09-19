30442c1bbe26ad4ccf617d7871a1efdbf08ffd9c
kyle uyehara <kuyehara1@gmail.com>
2016-09-18 23:53:55 -1000

    more parsers for phone number

--- validatePhoneNum.js
+++ validatePhoneNum.js
@@ -1,13 +1,13 @@
 function telephoneCheck(phoneNum) {
-  // Good luck!
-  // Input validation
   var plainNumber = parseNumerals(phoneNum);
-  console.log(plainNumber.length);
+
+  //Input validation
   if(typeof phoneNum !== 'string') {
     var err1 = new Error('Oops! Expected a string and instead received a ' + typeof str);
     throw err1;
   }
 
+  //Checks for number length with or withou country code 1
   if(plainNumber.length !== 10) {
     if(plainNumber.length === 11) {
       if(plainNumber[0] !== '1') {
@@ -18,21 +18,28 @@ function telephoneCheck(phoneNum) {
     return false;
   }
 
-  console.log(plainNumber);
-
   function parseNumerals(phoneNum) {
     var str = '';
     for(var i = 0; i < phoneNum.length; i++) {
+      if(phoneNum[i].charCodeAt() === 40) {
+        if(phoneNum[i + 4].charCodeAt() !== 41) {
+          return false;
+        }
+      }
+      if(phoneNum[i].charCodeAt() === 41) {
+        console.log(i);
+        if(phoneNum[i - 4].charCodeAt() !== 40) {
+          return false;
+        }
+      }
       if(phoneNum[i].charCodeAt() > 47) {
         str += (phoneNum[i]);
       }
     }
-    console.log('str', str);
     return str;
   }
 }
 
+var check = telephoneCheck("555)-555-5555");
 
-
-var check = telephoneCheck("1 (757) 622-7382");
 console.log('check', check);
