0398d63a2f1ad8bf006f65356eb25da042c7244a
kyle uyehara <kuyehara1@gmail.com>
2016-09-18 23:25:19 -1000

    implement number parsing

--- validatePhoneNum.js
+++ validatePhoneNum.js
@@ -1,24 +1,38 @@
-function telephoneCheck(str) {
+function telephoneCheck(phoneNum) {
   // Good luck!
   // Input validation
-  var plainNumber = '';
-
-  if(typeof str !== 'string') {
+  var plainNumber = parseNumerals(phoneNum);
+  console.log(plainNumber.length);
+  if(typeof phoneNum !== 'string') {
     var err1 = new Error('Oops! Expected a string and instead received a ' + typeof str);
     throw err1;
   }
 
-  for(var i = 0; i < str.length; i++) {
-    if(str[i].charCodeAt() > 47) {
-      plainNumber.concat(str[i]);
+  if(plainNumber.length !== 10) {
+    if(plainNumber.length === 11) {
+      if(plainNumber[0] !== '1') {
+        return false;
+      }
+      return;
     }
+    return false;
   }
 
   console.log(plainNumber);
 
-  return true;
+  function parseNumerals(phoneNum) {
+    var str = '';
+    for(var i = 0; i < phoneNum.length; i++) {
+      if(phoneNum[i].charCodeAt() > 47) {
+        str += (phoneNum[i]);
+      }
+    }
+    console.log('str', str);
+    return str;
+  }
 }
 
 
 
-telephoneCheck("0 (757) 622-7382");
+var check = telephoneCheck("1 (757) 622-7382");
+console.log('check', check);
