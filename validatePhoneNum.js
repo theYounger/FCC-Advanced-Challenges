a05c41b705ca05484d5236a393b5df014d90289a
kyle uyehara <kuyehara1@gmail.com>
2016-09-19 12:58:43 -1000

    implement regexp for improved search utility

--- validatePhoneNum.js
+++ validatePhoneNum.js
@@ -1,5 +1,5 @@
 function telephoneCheck(phoneNum) {
-  var plainNumber = parseNumerals(phoneNum);
+  // var plainNumber = parseNumerals(phoneNum);
 
   //Input validation
   if(typeof phoneNum !== 'string') {
@@ -7,39 +7,82 @@ function telephoneCheck(phoneNum) {
     throw err1;
   }
 
-  //Checks for number length with or withou country code 1
-  if(plainNumber.length !== 10) {
-    if(plainNumber.length === 11) {
-      if(plainNumber[0] !== '1') {
+  /*=============
+  =====WITH REGEX
+  ==============*/
+  var numeralsOnly = phoneNum.match(/\d/g);
+  var noSpaces = phoneNum.replace(/\s/g, '');
+  var openParen = noSpaces.search(/\(/g);
+  var closeParen = noSpaces.search(/\)/g);
+  var hypen = noSpaces.search(/\-/g);
+
+  //10-length branch
+  if(numeralsOnly.length === 10) {
+    //check for open parens at specific points
+    if(openParen !== -1 && closeParen !== -1) {
+      if(openParen !== 0 && closeParen !== 4) {
         return false;
       }
       return;
     }
-    return false;
+    //check for hypens at specific points
+
   }
 
-  function parseNumerals(phoneNum) {
-    var str = '';
-    for(var i = 0; i < phoneNum.length; i++) {
-      if(phoneNum[i].charCodeAt() === 40) {
-        if(phoneNum[i + 4].charCodeAt() !== 41) {
-          return false;
-        }
+  //11-length branch
+  if(numeralsOnly.length === 11) {
+    if(openParen !== -1 && closeParen !== -1) {
+      if(openParen !== 1 && closeParen !== 5) {
+        return false;
       }
-      if(phoneNum[i].charCodeAt() === 41) {
-        console.log(i);
-        if(phoneNum[i - 4].charCodeAt() !== 40) {
-          return false;
-        }
-      }
-      if(phoneNum[i].charCodeAt() > 47) {
-        str += (phoneNum[i]);
-      }
+      return;
     }
-    return str;
   }
 }
 
-var check = telephoneCheck("555)-555-5555");
+
+
+
+
+
+
+  /*=============
+  ======W/O REGEX
+  ==============*/
+
+  //Checks for number length with or withou country code 1
+//   if(plainNumber.length !== 10) {
+//     if(plainNumber.length === 11) {
+//       if(plainNumber[0] !== '1') {
+//         return false;
+//       }
+//       return;
+//     }
+//     return false;
+//   }
+
+//   function parseNumerals(phoneNum) {
+//     var str = '';
+//     for(var i = 0; i < phoneNum.length; i++) {
+//       if(phoneNum[i].charCodeAt() === 40) {
+//         if(phoneNum[i + 4].charCodeAt() !== 41) {
+//           return false;
+//         }
+//       }
+//       if(phoneNum[i].charCodeAt() === 41) {
+//         console.log(i);
+//         if(phoneNum[i - 4].charCodeAt() !== 40) {
+//           return false;
+//         }
+//       }
+//       if(phoneNum[i].charCodeAt() > 47) {
+//         str += (phoneNum[i]);
+//       }
+//     }
+//     return str;
+//   }
+// }
+
+var check = telephoneCheck("0 (757) 622-7382");
 
 console.log('check', check);
