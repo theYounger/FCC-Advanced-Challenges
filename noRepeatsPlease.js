31240d12b405d76facc2348d5abe2e0c68585344
kyle uyehara <kuyehara1@gmail.com>
2016-09-20 00:01:21 -1000

    input validation

--- noRepeatsPlease.js
+++ noRepeatsPlease.js
@@ -1,4 +1,10 @@
 function permAlone(str) {
+  //Input validation
+  if(typeof str !== 'string') {
+    var err = new Error('Oops! We expected a string, but instead saw a ' + typeof str);
+    throw err;
+  }
+
 
   return str;
 }
