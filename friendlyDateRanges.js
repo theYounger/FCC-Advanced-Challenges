e66e803464741d74a1ec9e9397b03c7588f64d69
kyle uyehara <kuyehara1@gmail.com>
2016-09-19 16:43:26 -1000

    implement parsing of dates to ordinal vs cardinal

--- friendlyDateRanges.js
+++ friendlyDateRanges.js
@@ -19,12 +19,64 @@ function makeFriendlyDates(arr) {
   var monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   var specialDaySuffix = [01, 02, 03, 21, 22, 23, 31];
   var dateArrayBegin = arr[0].split('-');
-  var beginMonth = Number(dateArrayBegin);
   var dateArrayEnd = arr[1].split('-');
+  var beginMonth = Number(dateArrayBegin[1]);
+  var endMonth = Number(dateArrayEnd[1]);
 
-  console.log(beginMonth);
+  //Convert month number to name
+  dateArrayBegin[1] = monthList[beginMonth - 1];
+  dateArrayEnd[1] = monthList[endMonth - 1];
+  dateArrayBegin[2] = ordinalize(dateArrayBegin[2]);
+  dateArrayEnd[2] = ordinalize(dateArrayEnd[2]);
 
-  return arr;
+  return finalConcat(dateArrayBegin, dateArrayEnd);
+
+  function finalConcat(begin, end) {
+    var finalArr;
+
+
+
+    //Check if within same year and month
+    if(begin[0] === end[0] && begin[0] === '2016') {
+      if(begin[1] === end[1]) {
+        begin[0] = null; end[0] = null; end[1] = null;
+      }
+    }
+    if((end[0] - begin[0]) === 1 && (endMonth - beginMonth) <= 0) {
+      if((endMonth - beginMonth) === 0) {
+        if(end[2] - begin[2] <= 0) {
+          begin[0] = null; end[0] = null; end[1] = null;
+        }
+      }
+      begin[0]
+    }
+
+
+    finalArr = [`${begin[1]} ${begin[2]}, ${begin[0]}`, `${end[1]} ${end[2]}, ${end[0]}`];
+    return finalArr;
+  }
+
+  function ordinalize(cardinalDay) {
+    switch(cardinalDay) {
+      case '01':
+        return '1st';
+      case '02':
+        return '2nd';
+      case '03':
+        return '3rd';
+      case '21':
+        return '21st';
+      case '22':
+        return '22nd';
+      case '23':
+        return '23rd';
+      case '31':
+        return '31st';
+      default:
+        var num = Number(cardinalDay);
+        return num + 'th';
+    }
+  }
 }
 
-makeFriendlyDates(['2016-07-01', '2016-07-04']);
+console.log(makeFriendlyDates(['2016-07-01', '2016-07-04']));
