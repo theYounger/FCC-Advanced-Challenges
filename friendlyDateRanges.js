a7af95f76dffd996466a1c2d15ef347b73be052a
kyle uyehara <kuyehara1@gmail.com>
2016-09-19 23:20:57 -1000

    pass 90% of tests, two more to go

--- friendlyDateRanges.js
+++ friendlyDateRanges.js
@@ -1,63 +1,67 @@
-
 function makeFriendlyDates(arr) {
   //Input validation
   if(!Array.isArray(arr)) {
     var err1 = new Error('Oops! We were expecting an array, but received a ' + typeof arr);
     throw err1;
   }
-
   if(arr.length !== 2) {
     var err2 = new Error('Oops! We were expecting two array elements, but received ' + arr.length);
     throw err2;
   }
-
   if(typeof arr[0] !== 'string' || typeof arr[1] !== 'string') {
     var err3 = new Error('Oops! We were expecting both elements to be strings');
     throw err3;
   }
 
-  var monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  var specialDaySuffix = [01, 02, 03, 21, 22, 23, 31];
-  var dateArrayBegin = arr[0].split('-');
-  var dateArrayEnd = arr[1].split('-');
-  var beginMonth = Number(dateArrayBegin[1]);
-  var endMonth = Number(dateArrayEnd[1]);
+  var dateBeginArr = arr[0].split('-');
+  var dataEndArr = arr[1].split('-');
+  var beginMo = Number(dateBeginArr[1]);
+  var endMo = Number(dataEndArr[1]);
+  var moList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   //Convert month number to name
-  dateArrayBegin[1] = monthList[beginMonth - 1];
-  dateArrayEnd[1] = monthList[endMonth - 1];
-  dateArrayBegin[2] = ordinalize(dateArrayBegin[2]);
-  dateArrayEnd[2] = ordinalize(dateArrayEnd[2]);
+  dateBeginArr[1] = moList[beginMo - 1];
+  dataEndArr[1] = moList[endMo - 1];
 
-  return finalConcat(dateArrayBegin, dateArrayEnd);
+  return ordinalizeDate(dateBeginArr, dataEndArr);
 
-  function finalConcat(begin, end) {
-    var finalArr;
-
-
+  function ordinalizeDate(begin, end) {
+    var ordinalDate;
+    var beginYear = ', ' + begin[0];
+    var endYear = ', ' + end[0];
+    var endMonth = end[1] + ' ';
 
     //Check if within same year and month
-    if(begin[0] === end[0] && begin[0] === '2016') {
-      if(begin[1] === end[1]) {
-        begin[0] = null; end[0] = null; end[1] = null;
+    if(end[0] - begin[0] === 0) {
+      endYear = '';
+      if(endMo - beginMo === 0) {
+        endMonth = '';
       }
     }
-    if((end[0] - begin[0]) === 1 && (endMonth - beginMonth) <= 0) {
-      if((endMonth - beginMonth) === 0) {
-        if(end[2] - begin[2] <= 0) {
-          begin[0] = null; end[0] = null; end[1] = null;
+    if(end[0] - begin[0] === 1) {
+      if(endMo - beginMo < 0) {
+        endYear = '';
+      }
+      if(endMo - beginMo === 0) {
+        if(end[2] - begin[2] < 0) {
+          endYear = ''; end[1] = '';
         }
       }
-      begin[0]
+    }
+    if(begin[0] === '2016') {
+      beginYear = '';
     }
 
+    begin[2] = ordinalizeDay(begin[2]);
+    end[2] = ordinalizeDay(end[2]);
 
-    finalArr = [`${begin[1]} ${begin[2]}, ${begin[0]}`, `${end[1]} ${end[2]}, ${end[0]}`];
-    return finalArr;
+    ordinalDate = [`${begin[1]} ${begin[2]}${beginYear}`, `${endMonth}${end[2]}${endYear}`];
+    console.log(begin, end);
+    return ordinalDate;
   }
 
-  function ordinalize(cardinalDay) {
-    switch(cardinalDay) {
+  function ordinalizeDay(cardinalDate) {
+    switch(cardinalDate) {
       case '01':
         return '1st';
       case '02':
@@ -73,10 +77,9 @@ function makeFriendlyDates(arr) {
       case '31':
         return '31st';
       default:
-        var num = Number(cardinalDay);
-        return num + 'th';
+        return Number(cardinalDate) + 'th';
     }
   }
 }
 
-console.log(makeFriendlyDates(['2016-07-01', '2016-07-04']));
+console.log(makeFriendlyDates(["2016-12-01", "2017-02-03"]) );
